b45fe0dee00910fedbe7bf3e4ff005a839320422
A-Meteor-in-Feb <yang.diandian@hotmail.com>
2025-01-02 18:19:31 +0800

    command center in process

---
+++ cyclonev2.1/command_center/center_publisher.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <typeinfo>
+#include <utility>
+
+#include "shutdownsignal.hpp"
+#include "dds/dds.hpp"
+#include "ControlData.hpp"
+
+using namespace org::eclipse::cyclonedds;
+
+
+void run_publisher_application(const std::string& tele_id, const std::string& vehicle_id, std::atomic<bool>& p_conn) {
+
+}
--- cyclonev2.1/command_center/center_subscriber.cpp
+++ cyclonev2.1/command_center/center_subscriber.cpp
@@ -1,51 +1,111 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
-
+#include <queue>
 #include <typeinfo>
-
+#include <utility>
+#include <map>
+#include <TCorePolicy.hpp>
 #include "shutdownsignal.hpp"
 #include "dds/dds.hpp"
 #include "ControlData.hpp"
 
 using namespace org::eclipse::cyclonedds;
 
-std::map<std::string, std::string> vehicle_map;
-std::map<std::string, std::string> tele_map;
-std::map<std::string, std::string> tele_vehicle_pair;
 
-//add the potential pairs into the tele_vehicle_pair.
-void judge_connection(std::atomic<bool>& p_conn) {
+std::vector<std::string> online_tele;
+std::vector<std::string> online_vehicle;
+std::map<std::string, std::string> con_te_ve;
 
-	bool vehicle_usable = false;
-	bool tele_usable = false;
 
-	std::map<std::string, std::string> ::iterator vehicle_iter;
-	std::map<std::string, std::string> ::iterator tele_iter;
-	std::map<std::string, std::string> ::iterator tele_vehicle_iter;
+//publish the connection message in strictly reliable way...
+//Also, publisher will only publish the connection msg to the relevant subscribers,
+//so, we need set Partition Qos, then do the initialization of Data Writer.
+//Use the QoS settings.
+void run_publisher_application(dds::pub::Publisher& command_publisher, dds::topic::Topic<ControlData::connection_msg>& con_topic, const std::string& tele_id, const std::string& vehicle_id) {
 
-	for (tele_iter = tele_map.begin(); tele_iter != tele_map.end(); tele_iter++) {
+	dds::pub::qos::DataWriterQos w_qos;
+	w_qos << dds::core::policy::detail::Reliability(dds::core::policy::ReliabilityKind_def::RELIABLE);
+	w_qos << dds::core::policy::detail::History(dds::core::policy::HistoryKind_def::KEEP_ALL);
 
-		if (tele_iter->second == "online") {
+	if (tele_id == "non-mateched") {
+		w_qos << dds::core::policy::detail::Partition(dds::core::policy::detail::TPartition({ vehicle_id }));
+	}
+	else if (vehicle_id = "non-matched") {
+		w_qos << dds::core::policy::detail::Partition(dds::core::policy::detail::TPartition({ tele_id }));
+	}
+	else {
+		w_qos << dds::core::policy::detail::Partition(dds::core::policy::detail::TPartition({ tele_id, vehicle_id }));
+	}
+	
+	dds::pub::DataWriter con_topic_writer(command_publisher, con_topic, w_qos);
 
-			for (vehicle_iter = vehicle_map.begin(); vehicle_iter != vehicle_map.end(); vehicle_iter++) {
+	ControlData::connection_msg con_msg(tele_id, vehicle_id);
+	con_topic_writer.write(con_msg);
+}
 
-				if (vehicle_iter->second == "online") {
 
-				}
-			}
+
+//add the potential pairs into the tele_vehicle pair.
+void judge_connection(dds::pub::Publisher& command_publisher, dds::topic::Topic<ControlData::connection_msg>& con_topic) {
+
+	if (!online_tele.empty() || !online_vehicle.empty()) {
+
+		int tele_num = online_tele.size();
+		int vehicle_num = online_vehicle.size();
+
+		while ( tele_num && vehicle_num) {
+
+			std::string usable_vehicle = online_vehicle.back();
+			std::string usable_tele = online_tele.back();
+
+			online_vehicle.pop_back();
+			online_tele.pop_back();
+
+			tele_num -= 1;
+			vehicle_num -= 1;
+
+			con_te_ve.insert(std::make_pair(usable_tele, usable_vehicle));
+			con_te_ve.insert(std::make_pair(usable_vehicle, usable_tele));
+
+			run_publisher_application(command_publisher, con_topic, usable_tele, usable_vehicle);
 		}
+
+		while (tele_num) {
+			std::string usable_tele = online_tele.back();
+			std::string usable_vehicle = "non-matched";
+			tele_num -= 1;
+
+			run_publisher_application(command_publisher, con_topic, usable_tele, usable_vehicle);
+		}
+
+		while (vehicle_num) {
+			std::string usable_vehicle = online_vehicle.back();
+			std::string usable_tele = "non-matched";
+			vehicle_num -= 1;
+
+			run_publisher_application(command_publisher, con_topic, usable_tele, usable_vehicle);
+		}
+		
 	}
 
 	
-
 }
+
 
 void run_subscriber_application(std::atomic<bool>& p_conn) {
 
 	int command_domain = 0;
 
 	dds::domain::DomainParticipant command_participant(command_domain);
+
+
+	//---- initialize publisher and data writer and related topics
+	dds::pub::Publisher command_publisher(command_participant);
+
+	dds::topic::Topic<ControlData::connection_msg> con_topic(command_participant, "connection_msg");
+
+
+	//---- initializa subscriber and data readers and related topics
 	dds::sub::Subscriber command_subscriber(command_participant);
 
 	dds::topic::Topic<ControlData::tele_status> tele_status_topic(command_participant, "tele_status_data");
@@ -61,6 +121,7 @@ void run_subscriber_application(std::atomic<bool>& p_conn) {
 	while (!shutdown_requested) {
 
 		tele_status_samples = tele_status_reader.take();
+
 		if (tele_status_samples.length() > 0) {
 
 			dds::sub::LoanedSamples<ControlData::tele_status>::const_iterator iter;
@@ -74,51 +135,30 @@ void run_subscriber_application(std::atomic<bool>& p_conn) {
 					std::cout << "tele_status data: " << data << std::endl;
 
 					std::string tele_id = data.tele_id();
+					std::string tele_state = data.connected() && data.online() ? "connected" : "online";
+					tele_state = data.online() && !data.connected() ? "online" : "offline";
 
-					if (tele_vehicle_pair.count(tele_id)) {
+					if (tele_state == "online") {
+						bool usable = true;
 
-						std::string tele_state = "connected";
-						
-						if (!data.connected()) {
-
-							if (data.online()) {
-
-								std::string vehicle = tele_vehicle_pair[tele_id];
-
-								if (vehicle_map[vehicle] != "offline" ) {
-									//transmit the connection msg again.
-								}
-								else {
-									//delete this pair, set tele_state = "online"
-									//transmit the disconnection msg.
-								}
-							}
-							else {
-								//judge vehicle's state, unless it's offline, make it be online.
-								//delete the pair.
-								//transmit the disconnection msg.
-							}
-						}
-						
-
-					}else {
-
-						std::string tele_state = "offline";
-
-						if (data.online()) {
-							tele_state = "online";
-							if (tele_map.count(tele_id) == 0) {
-								tele_map.insert(std::make_pair(tele_id, tele_state));
-							}
-						}
-						else {
-							if (tele_map.count(tele_id) != 0) {
-								tele_map.erase(tele_id);
+						for (int i = 0; i < online_tele.size(); i++) {
+							if (online_tele.at(i) == tele_id) {
+								usable = false;
+								break;
 							}
 						}
 
-						
+						if (usable) {
+							online_tele.push_back(tele_id);
+						}
 					}
+
+					if (tele_state == "offline" && con_te_ve.count(tele_id)) {
+						//send dis-connection msg. 
+						//delete tele_id:vehicle_id && vehicle_id:tele_id pairs.
+					}
+					
+					
 
 				}
 
@@ -126,6 +166,7 @@ void run_subscriber_application(std::atomic<bool>& p_conn) {
 		}
 
 		vehicle_status_samples = vehicle_status_reader.take();
+
 		if (vehicle_status_samples.length() > 0) {
 
 			dds::sub::LoanedSamples<ControlData::vehicle_status>::const_iterator iter;
@@ -139,28 +180,34 @@ void run_subscriber_application(std::atomic<bool>& p_conn) {
 					std::cout << "vehicle_status data: " << data << std::endl;
 
 					std::string vehicle_id = data.vehicle_id();
-					std::string vehicle_state = "offline";
+					std::string vehicle_state = data.online() && data.connected() ? "connected" : "online";
+					vehicle_state = data.online && !data.connected() ? "online" : "offline";
 
-					//judge the state and add this vehicle to the corresponding map.
-					if (data.connected()) {
-						vehicle_state = "connected";
-					}
-					else if (data.online()) {
-						vehicle_state = "online";
+					if (vehicle_state == "online") {
+						bool usable = true;
+
+						for (int i = 0; i < online_vehicle.size(); i++) {
+							if (online_vehicle.at(i) == vehicle_id) {
+								usable = false;
+								break;
+							}
+						}
+
+						if (usable) {
+							online_vehicle.push_back(vehicle_id);
+						}
 					}
 
-					if (vehicle_map.count(vehicle_id)) {
-						vehicle_map[vehicle_id] = vehicle_state;
-					}
-					else {
-						vehicle_map.insert(std::make_pair(vehicle_id, vehicle_state));
+					if (vehicle_state == "offline" && con_te_ve.count(vehicle_id)) {
+						//send dis-connection msg. 
+						//delete tele_id:vehicle_id && vehicle_id : tele_id pairs.
 					}
 				}
 
 			}
 		}
 
-
+		judge_connection(command_publisher, con_topic);
 
 	}
 }
